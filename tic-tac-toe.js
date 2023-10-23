f5b92abf9d9e43c3e15309401d77e54ef781c1a8
AS-Coope <deltazirconium@gmail.com>
2023-10-22 23:34:03 -0500

    Set class of each div in game board to square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     //ensures the html loads before javascript is ran
+
+    let gameBoard = document.getElementById("board");
+
+    for (let square of gameBoard.children) {
+        square.setAttribute("class", "square");
+    }
+
 });
