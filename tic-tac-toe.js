b9296ef6dc26ede3c37747756da02d8312bf1ec5
AS-Coope <deltazirconium@gmail.com>
2023-10-23 02:07:21 -0500

    Add functionality to switch target printed to console when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,36 +1,28 @@
+// global variables
+let markerSwitchValue = 0;
+let targetValue = "X"
+
+let playerClick = function () {
+    targetValue = (markerSwitchValue === 0) ? "X" : "O";
+    console.log(targetValue);
+    markerSwitchValue = (markerSwitchValue === 0) ? 1 : 0;
+};
+
 let documentReady = function () {
     let gameBoard = document.getElementById("board");
     let element = gameBoard.querySelector("div");
-    let htmlGameBoardArray = [];
+    let iterator = 0;
 
     for (let square of gameBoard.children) {
         square.classList.add("square");
-        square.setAttribute("onclick", "playerClick(target, elem)");
-        htmlGameBoardArray.push(square);
+        square.classList.add(`${iterator}`);
+        square.setAttribute("onclick", "playerClick()");
+        iterator += 1;
     }
-
-    console.log(htmlGameBoardArray);
 
     let gameBoardArray = ["", "", "", "", "", "", "", "", ""];
     let gameEnd = false;
-    let markerSwitchValue = 0;
-    let targetValue = "X";
-
-    let playerClick = function (target, elem) {
-        elem.textContent = target;
-    };
-
-    while (gameEnd !== true) {
-        targetValue = (markerSwitchValue === 0) ? "X" : "O";
-
-
-        playerClick(targetValue, element);
-        // ensures target switches to opposite target than what was just used
-        markerSwitchValue = (markerSwitchValue === 0) ? 1 : 0;
-        gameEnd = true; // here to prevent browser crashing from while loop running infinitely
-    }
 }
-
 //ensures the html loads before javascript is ran
 document.addEventListener('DOMContentLoaded', (event) => {
     documentReady();
